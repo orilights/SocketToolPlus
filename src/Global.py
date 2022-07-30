84cbbb50a2c1769a751db5cffd8e70f28a629436
OriLight <61224409+OriLight152@users.noreply.github.com>
2022-07-30 17:18:06 +0800

    Update 1.1.1 Beta

--- src/Global.py
+++ src/Global.py
@@ -4,5 +4,5 @@ ROOT_PATH = os.path.dirname((os.path.abspath(__file__)))
 LOCALHOST = '127.0.0.1'
 ENCODING_SOCKET = 'gb18030'
 ENCODING_FILE = 'utf-8'
-VERSION_STR = '1.1.0 Beta'
-VERSION_NO = '1220730'
+VERSION_STR = '1.1.1 Beta'
+VERSION_NO = '122073001'
